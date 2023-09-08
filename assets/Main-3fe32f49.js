46a204f4dd435ef4a9518b8fc8c0c315cf31bc6d
Arillaxe <bekirshabut66@gmail.com>
2023-09-08 21:16:11 +0000

    Deploying to gh-pages from @ Obschak/frontend@6de837b6dd34f16daab5c3740a05ed7c2d84df07 🚀

---
+++ assets/Main-3fe32f49.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b241cd42.js";const e=()=>r.jsx("div",{children:"MAIN PAGE"});export{e as default};
--- assets/Main-d1e41593.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-97b3c795.js";const e=()=>r.jsx("div",{children:"MAIN PAGE"});export{e as default};
