92f03876430635c6b1ee6bbbea50b1a376732509
sensej <65352726+sensej@users.noreply.github.com>
2023-09-08 21:06:09 +0000

    Deploying to gh-pages from @ Obschak/frontend@19f4afab4f59468d9bff3d025365164d7eb25cfd 🚀

--- assets/Main-53dcc8af.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-93190662.js";const e=()=>r.jsx("div",{children:"MAIN PAGE"});export{e as default};
---
+++ assets/Main-d1e41593.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-97b3c795.js";const e=()=>r.jsx("div",{children:"MAIN PAGE"});export{e as default};
