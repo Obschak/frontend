f2843a4b4a09ec294f842ae3bfc1fa2d209778a3
Ramazan Vlilyaev <itsensej@gmail.com>
2023-09-14 12:55:20 +0300

    maked toast component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,21 @@
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { PlusIcon } from '@radix-ui/react-icons';
 
 import Avatar from '../../components/Avatar';
-
-import styles from './styles.module.scss';
+import Toast from '../../components/Toast';
 import { useAppSelector } from '../../store/hooks';
 
+import styles from './styles.module.scss';
+
 const MainPage = () => {
+  const [openToast, setOpenToast] = useState(false);
+  const timerRef = useRef(0);
   const { picture } = useAppSelector(({ auth }) => auth);
+
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current);
+  }, []);
 
   return (
     <div className={styles.main}>
@@ -23,6 +31,21 @@ const MainPage = () => {
         <PlusIcon width="80" height="80" />
         <p>Создать общак</p>
       </Link>
+      <button
+        onClick={() => {
+          setOpenToast(false);
+          timerRef.current = window.setTimeout(() => {
+            setOpenToast(true);
+          }, 100);
+        }}
+      >
+        Show toast
+      </button>
+      <Toast
+        message={'Ссылка скопирована в буфер обмена!'}
+        openToast={openToast}
+        setOpenToast={setOpenToast}
+      />
     </div>
   );
 };
