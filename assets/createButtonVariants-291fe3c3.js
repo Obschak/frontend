e15c06df26b86bf6360d30bd9065dc4f01ff3a3c
Arillaxe <bekirshabut66@gmail.com>
2023-09-17 16:45:31 +0000

    Deploying to gh-pages from @ Obschak/frontend@69fa1dcd2f24ba7c839ac53267fa07a5068a374c 🚀

---
+++ assets/createButtonVariants-291fe3c3.js
@@ -0,0 +1,1 @@
+const a={hidden:{opacity:0,y:100},visible:{opacity:1,y:0,transition:{duration:.8,delay:.2,ease:[0,.71,.2,1.01]}},whileTap:{scale:.95}};export{a as c};
---
+++ assets/headingVariants-7ad919fa.js
@@ -0,0 +1,1 @@
+const a={hidden:{opacity:0,scale:.5},visible:{opacity:1,scale:1,transition:{duration:.8,delay:.2,ease:[0,.71,.2,1.01]}}};export{a as h};
